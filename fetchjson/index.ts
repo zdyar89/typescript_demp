e1d2d8c7fa720b81ec5ac9b781081f4c435392c6
zdyar89 <zdyar89@gmail.com>
2023-08-13 09:38:54 -0400

    added console func, todo interface

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,27 @@ import axios from "axios";
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(Response => {
-  console.log(Response.data);
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+axios.get(url).then(response => {
+
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The ToDO with ID: ${id}
+    Has a title of: ${title}
+    Is it finished?: ${completed}
+  `);
+};
